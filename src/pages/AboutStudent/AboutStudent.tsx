2ce282a74214d143afba0311e5db2a7f7e331fa3
vinicius2 <viniciussantosfrota10012017@gmail.com>
2024-03-04 16:52:04 -0300

    Main

--- src/pages/AboutStudent/AboutStudent.tsx
+++ src/pages/AboutStudent/AboutStudent.tsx
@@ -1,12 +1,50 @@
 import { LoggedNavbar } from "../../components/Navbar/LogedNavbar/LoggedNavbar";
-import { AboutStudentContainer } from "./Styles";
+import { AboutStudentContainer, Wallpaper, SideBar, MainContent } from "./Styles";
+import background from '../../assets/admLogo.png'
+import trophy from './images/trophy.png'
+import { ArrowRight, FileSearch } from "@phosphor-icons/react";
 
-export function AboutStudent(){
-     return(
+export function AboutStudent() {
+     return (
           <>
                <LoggedNavbar />
                <AboutStudentContainer>
-                    <h2>estudante</h2>
+                    <Wallpaper>
+                         <img src={background} alt="adm" />
+                    </Wallpaper>
+
+                    <SideBar>
+                         <header>
+                              <img src={trophy} alt="" />
+                              <h2>Turma de Adm</h2>
+                         </header>
+                         <nav>
+                              <p>
+                                   Conquistas
+                                   <ArrowRight />
+                              </p>
+                              <p>
+                                   Destaque
+                                   <ArrowRight />
+                              </p>
+                              <p>
+                                   Formatura
+                                   <ArrowRight />
+                              </p>
+                         </nav>
+                    </SideBar>
+                    <MainContent>
+                         <header>
+                              <label >
+                                   <input type="text"
+                                        id="s"
+                                        placeholder="Pesquisar"
+                                   />
+                                   <FileSearch />
+                              </label>
+                              hello
+                         </header>
+                    </MainContent>
                </AboutStudentContainer>
           </>
      )
--- src/pages/AboutStudent/Styles.ts
+++ src/pages/AboutStudent/Styles.ts
@@ -2,6 +2,83 @@ import styled from "styled-components";
 
 
 export const AboutStudentContainer = styled.div`
-     
+
+`
+
+export const Wallpaper = styled.div`
+     width: 100%;
+     height: 16rem;
+
+     display: flex;
+     justify-content: center;
+     align-items: center;
+
+     background-color: none;
+
+     img{
+          width: 20%;
+          height: auto;
+          display: flex;
+          margin: auto;
+     }
+
+`
+export const SideBar = styled.aside`
+     width: 25rem;
+     height: 60vh;
+
+     background: ${props => props.theme['global']};
+     margin-top: -2rem;
+     padding: 1rem 2rem;
+
+     display: flex;
+     flex-direction: column;
+
+     header{
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          gap: 1rem;
+
+          img{
+               width: 20%;
+               height: auto;
+          }
+          h2{
+               color: ${props => props.theme['white']};
+               font: 400 normal 2rem;
+               margin: 0 auto 0  1rem;
+          }
+     }
+     nav{
+          display: flex;
+          flex-direction: column;
+          padding: 2rem 1rem;
+          color: ${props => props.theme['white']};
+          line-height: 2rem;
+          font-size: 1.2rem;
+
+          p{
+               display: flex;
+               align-items: center;
+               gap: 1rem;
+               margin-left: 0;
+          }
+     }
+
+
+`
+export const MainContent = styled.main`
+     background: ${props => props.theme['white']};;
+
+     padding: 2rem;
+     width: auto;
+     height: 60vh;
+
+
+     header{
+          height: 20%;
+          background: royalblue;
+     }
 
 `
